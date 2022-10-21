faa6330bb47f38474e3f847351d3ea613df995f6
Divya <divyanarhe@gmail.com>
2022-10-21 15:55:48 +0530

    INitial commit

---
+++ includes/class-wp-book-deactivator.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://example.com
+ * @since      1.0.0
+ *
+ * @package    Wp_Book
+ * @subpackage Wp_Book/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Book
+ * @subpackage Wp_Book/includes
+ * @author     Divya <divya.narhe@gmail.com>
+ */
+class Wp_Book_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
